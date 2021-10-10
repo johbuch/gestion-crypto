41be4b8ddbc5092d60b5abf3b76ad8ae069d5572
johbuch <johan.buchert@gmail.com>
2021-10-10 18:16:05 +0200

    WIP ajout tableau

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,31 @@
 @include('layout._head')
-
     <div>
-        <h1>Accueil</h1>
+        <h1>Stat de suivi ETH</h1>
+        <table class="table table-responsive table-hover">
+            <thead class="table-head">
+                <tr>
+                    <th scope="col">ETH</th>
+                    <th scope="col">Prix d'achat</th>
+                    <th scope="col">Valeur ETH lors de l'Achat</th>
+                    <th scope="col">Nombre jetons</th>
+                    <th scope="col">Valeur ETH instant T</th>
+                    <th scope="col">Valeur en Euros instant T position</th>
+                    <th scope="col">Tx évolution valeur ETH en € (%)</th>
+                    <th scope="col">Gain - Loss</th>
+                </tr>
+            </thead>
+            <tbody class="table-body">
+                <tr>
+                    <td>18/05/2021</td>
+                    <td>9,92€</td>
+                    <td>2834,29 €</td>
+                    <td>0,0035</td>
+                    <td>3155</td>
+                    <td>11,04 €</td>
+                    <td>+ 11,35 %</td>
+                    <td><span class="text-success">+ 1,12 €</span></td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-
 @include('layout._footer')
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,3 +12,4 @@ const mix = require('laravel-mix');
  */
 mix.sass('resources/scss/app.scss', 'public/css');
 mix.js('resources/js/app.js', 'public/js');
+mix.browserSync('http://gestion-crypto.local/public/');

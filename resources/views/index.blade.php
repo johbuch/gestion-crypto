d49adfce85869eed8d8975ef96575195f7f1001d
johbuch <johan.buchert@gmail.com>
2021-10-12 19:54:13 +0200

    changement orientation tableau

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,47 @@
 @include('layout._head')
     <div>
-        <h1>Stat de suivi ETH</h1>
-        <table class="table table-responsive table-hover">
-            <thead class="table-head">
+        <div class="card card-eth table-responsive">
+            <h4 class="card-title">ETH</h4>
+            <table class="table table-responsive table-hover">
                 <tr>
-                    <th scope="col">ETH</th>
-                    <th scope="col">Prix d'achat</th>
-                    <th scope="col">Valeur ETH lors de l'Achat</th>
-                    <th scope="col">Nombre jetons</th>
-                    <th scope="col">Valeur ETH instant T</th>
-                    <th scope="col">Valeur en Euros instant T position</th>
-                    <th scope="col">Tx évolution valeur ETH en € (%)</th>
-                    <th scope="col">Gain - Loss</th>
-                </tr>
-            </thead>
-            <tbody class="table-body">
-                @foreach($ethDataDisplayed as $position)
-                    <tr class="text-center">
+                    <th>date</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>{{ $position['date_achat'] }}</td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Prix achat</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>{{ $position['prix_achat'] }}</td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Valeur ETH lors de l'Achat</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>{{ $position['valeur_eth_achat'] }}€</td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Nombre jetons</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>{{ $position['nombre_jeton'] }}</td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Valeur ETH instant T</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>{{ $priceEth  }}</td>
-                        <td><strong>{{ $position['instantT_Eur_valeur'] }}€</strong></td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Valeur en Euros instant T position</th>
+                    @foreach($ethDataDisplayed as $position)
+                        <td>{{ $position['instantT_Eur_valeur'] }}€</td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Tx évolution valeur ETH en € (%)</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>
                             @if(str_contains($position['taux_evo'], '+'))
                                 <span class="text-success">{{ $position['taux_evo'] }}</span>
@@ -30,6 +49,11 @@
                                 <span class="text-danger">{{ $position['taux_evo'] }}</span>
                             @endif
                         </td>
+                    @endforeach
+                </tr>
+                <tr>
+                    <th>Gain - Loss</th>
+                    @foreach($ethDataDisplayed as $position)
                         <td>
                             @if(str_contains($position['gain_loss'], "+"))
                                 <span class="text-success">{{ $position['gain_loss'] }} €</span>
@@ -37,9 +61,12 @@
                                 <span class="text-danger">{{ $position['gain_loss'] }} €</span>
                             @endif
                         </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                </tr>
+            </table>
+            <div>
+                Gain total : 100
+            </div>
+        <div>
     </div>
 @include('layout._footer')
